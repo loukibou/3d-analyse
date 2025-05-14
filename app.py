2a8fcf17a35d808c90bda75ea93af6b87fb5eb13
loukibou <95723062+loukibou@users.noreply.github.com>
2025-05-14 09:59:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -60,4 +60,6 @@ def analyze():
     })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    # Au lieu de port fixe, utilise celui injecté par Railway
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
